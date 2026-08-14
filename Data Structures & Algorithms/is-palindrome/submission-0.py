ed2f1de526b83424cec62863e7c4e448ae191bb6
Oluwagbemileke Adesiyan <gbemilekeadesiyan@gmail.com>
2026-08-14 13:21:09 -0400

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        cleaned = [c.lower() for c in s if c.isalnum()]
+        left = 0
+        right = len(cleaned) -1
+        while left < right:
+            if cleaned[left] != cleaned[right]:
+                return False 
+            else: 
+                left+=1
+                right-=1
+        return True 
